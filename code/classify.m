f56257e47b3237306bc661f82e7282375cd17af9
jimmylin192 <linxin@gmail.com>
2014-07-10 15:07:30 -0400

    initial setup

---
+++ code/classify.m
@@ -0,0 +1,15 @@
+function [c] = classify(XTrain, yTrain, XTest, h)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+% ------ START: REPLACE WITH YOUR CODE ------
+n = size(yTrain,1);
+nClass = size(yTrain,2)
+c = ones(n, nClass);
+% ------ END: REPLACE WITH YOUR CODE ------
+
+
+
+end
+
+
